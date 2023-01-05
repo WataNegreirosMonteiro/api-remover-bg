ef39afa94b9508bf1581d8339da3d1a82a62bc13
Wata Negreiros <90472705+WataNegreirosMonteiro@users.noreply.github.com>
2023-01-05 07:53:24 -0400

    att retorno em b64

--- api_bgremover.py
+++ api_bgremover.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 from rembg import remove
 from PIL import Image
+import base64
+import sys
+import json
 
 app = Flask(__name__)
 
@@ -10,5 +13,16 @@ def send_image():
   img = Image.open(file.stream)
   removing_bg = remove(img).save("foto.png")
   return send_file("./foto.png", as_attachment=True)
+
+
+@app.route('/remove_bg64', methods = ['PUT'])
+def send_image64():
+  file = request.files['image']
+  img = Image.open(file.stream)
+  removing_bg = remove(img).save("foto.png")
+  with open("foto.png", 'rb') as image_file:
+    data64 = base64.b64encode(image_file.read())
+  return json.dumps({ "image": str(data64).split("'")[1]})
+
 
 app.run(port=5000, host='0.0.0.0', debug=True)
